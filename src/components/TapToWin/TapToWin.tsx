eb85cbc7378f7e110d88d0d3aee3635f77f6a9f6
troonsply <troonsply@gmail.com>
2024-11-08 16:40:25 +0700

    add tooltip

--- src/components/TapToWin/TapToWin.tsx
+++ src/components/TapToWin/TapToWin.tsx
@@ -6,18 +6,32 @@ import ConnectWalletButton from '@/components/ConnectWalletButton/ConnectWalletB
 const TapToWin: FC = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-between bg-white py-2.5">
-            <Marquee text="Wallet ***jwp won 21 ETH! | Wallet ***nuf won 23 ETH! | Wallet ***583 won 0,99 ETH!" />
-            <ConnectWalletButton />
+            <Marquee text="Wallet ***jwp won 21 ETH! | Wallet ***nuf won 23 ETH! | Wallet ***583 won 0,99 ETH!"/>
+            <ConnectWalletButton/>
             <div className="text-center my-8 flex-col justify-between">
-                <h1 className="text-5xl text-black mb-40">Tap to win!</h1>
+                <div className="relative group">
+                    <h1 className="text-5xl text-black mb-40">Tap to win!</h1>
+                    {/* Tooltip */}
+                    <div
+                        className="text-left absolute bottom-full right-0 r mb-2 w-64 p-3 bg-gray-300 text-black text-sm rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <p className="font-semibold mb-1">Rules:</p>
+                        <span>- 3 different pools</span><br/>
+                        <span>- Each tap increases the total prize fund of the pool</span><br/>
+                        <span>
+                            - Random tap from 10 to 1000 receives the entire pool prize pool except for the 10%
+                                commission and transaction fee
+                        </span>
+                    </div>
+                </div>
                 <div className="flex space-x-16 mt-2">
-                    <TapButton label="1 ETH" />
-                    <TapButton label="0.1 ETH" />
-                    <TapButton label="0.01 ETH" />
+                    <TapButton label="1 ETH"/>
+                    <TapButton label="0.1 ETH"/>
+                    <TapButton label="0.01 ETH"/>
                 </div>
             </div>
 
-            <Marquee text="Wallet ***jwp tapped 1 ETH! | Wallet ***nuf tapped 0,1 ETH! | Wallet ***583 tapped 0,01 ETH!" />
+            <Marquee
+                text="Wallet ***jwp tapped 1 ETH! | Wallet ***nuf tapped 0,1 ETH! | Wallet ***583 tapped 0,01 ETH!"/>
         </div>
     );
 };
